ef36b73aacc1dfc9881c92dea49607a2e689a022
HojuShin <shin03188@naver.com>
2023-10-20 15:45:02 +0900

    chore: 구글 호스트 이미지 허용

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+const nextConfig = {
+    images: {
+        // Google 이미지 호스트
+        domains: ['lh3.googleusercontent.com'],
+    },
+};
+
+module.exports = nextConfig;
