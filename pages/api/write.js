7987b2350ece31bc875efd8ee825e980de43ca49
HojuShin <shin03188@naver.com>
2023-11-20 18:38:56 +0900

    Refactor: 날짜 처리

--- pages/api/write.js
+++ pages/api/write.js
@@ -5,18 +5,19 @@ import { authOptions } from "./auth/[...nextauth]";
 export default async function handler(request, response) {
 
     // 현재 로그인 유저 정보
-    let session = await getServerSession(request, response, authOptions);
+    let session = await getServerSession(request, response, authOptions)
     // 현재 날짜, 시간
-    let date = new Date()
+    let date = Date.now()
+    let current = new Date(date).getFullYear() + '-' +  (new Date(date).getMonth() + 1)  +  '-' +  new Date(date).getDate()
 
     if (session) {
-        request.body.author = session.user.email;
-        request.body.date = date ;
+        request.body.author = session.user.email
+        request.body.date = current 
     }
 
     if (request.method == 'POST') {
         let db = (await connectDB).db('Pro');
         let result = db.collection('post').insertOne(request.body);
-        response.redirect(302,  `/${ session.user.name}`)
+        response.redirect(302, `/${ session.user.name }`)
     }
 }
