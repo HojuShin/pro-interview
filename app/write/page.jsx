04f85c3af5c36747dc951eb5b60f1779e5866458
HojuShin <shin03188@naver.com>
2023-11-28 21:52:42 +0900

    feat: 폼 입력값의 존재 여부에 따라 submit 버튼 활성/비활성화 동적 처리

--- app/write/page.jsx
+++ app/write/page.jsx
@@ -1,14 +1,40 @@
+'use client'
+
 import '@/styles/write.css'
+import { useState } from 'react';
 
 export default function Write() {
+
+    const [question, setQuestion] = useState('');
+    const [answer, setAnswer] = useState('');
+
+    // question과 answer 중 하나라도 입력값 비어있으면 submit 버튼 비활성화
+    const isSubmitDisabled = !question || !answer;
+
+    // question 입력값 변경 시
+    const handleQuestionChange = (e) => {
+        setQuestion(e.target.value);
+    };
+
+    // answer 입력값 변경 시
+    const handleAnswerChange = (e) => {
+        setAnswer(e.target.value);
+    };
+
     return (
         <>
             <div className='wrtie'>
                 <form action='./api/write' method='POST' className='writeForm'>
                     <div className='write-flex'>
                         <div className='write-left'>
-                            <h4 variant="h4" tabindex="0">면접관의 예상질문을<br />작성해주세요.</h4>
-                            <textarea type='text' maxLength='200' name='question' placeholder='ex) 프로그래밍이란 뭐라고 생각하나요?' id='write-question-text'></textarea>
+                            <h4 variant="h4" tabIndex="0">면접관의 예상질문을<br />작성해주세요.</h4>
+                            <textarea
+                                type='text'
+                                maxLength='200'
+                                name='question'
+                                placeholder='ex) 프로그래밍이란 뭐라고 생각하나요?'
+                                id='write-question-text'
+                                onChange={handleQuestionChange}></textarea>
                         </div>
                         <div className='write-right'>
                             <div className='write-section'>
@@ -25,7 +51,13 @@ export default function Write() {
                                 <div className='write-answer' tabIndex="0">
                                     <h6 className='write-answer-text'>내가 준비한 답변을 입력해주세요. </h6><span className='option'> (공백포함 350자)</span>
                                     <hr className='write-answer-hr'></hr>
-                                    <textarea type='text' maxLength='350' name='answer' id='write-answer-input' placeholder='ex) 프로그래밍은 붕어빵입니다!'></textarea >
+                                    <textarea
+                                        type='text'
+                                        maxLength='350'
+                                        name='answer'
+                                        id='write-answer-input'
+                                        placeholder='ex) 프로그래밍은 붕어빵입니다!'
+                                        onChange={handleAnswerChange}></textarea >
                                 </div>
                                 <div className='write-section-option write-section-keyword '>
                                     <div className='write-section-keyword-text'><span className='hint'>Hint!</span> 키워드</div>
@@ -36,8 +68,13 @@ export default function Write() {
                                     </div>
                                 </div>
                                 <div className='submit-session'>
-                                    <button type="submit" className='submit-btn'>등록하기</button>
+                                    {/* 질문, 답변란 입력값 없으면 disabled */}
+                                    <button
+                                        type="submit"
+                                        className='submit-btn'
+                                        disabled={isSubmitDisabled}>등록하기</button>
                                 </div>
+
                             </div>
                         </div>
                     </div>
