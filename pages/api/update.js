38c35785be30424355d117807003ce5be3812a4b
HojuShin <shin03188@naver.com>
2023-12-08 00:12:29 +0900

    feat: 글 수정 업데이트 서버  기능 생성

---
+++ pages/api/update.js
@@ -0,0 +1,29 @@
+//글 수정 서버
+import { getServerSession } from "next-auth";
+import { authOptions } from "./auth/[...nextauth]";
+import { connectDB } from "@/db/dababase";
+import { ObjectId } from "mongodb";
+
+export default async function handler(request, response) {
+
+    let session = await getServerSession(request, response, authOptions)
+
+    if (request.method == 'POST') {
+        // 업데이트할 데이터 정의
+        let update = {
+            question: request.body.question,
+            answer: request.body.answer,
+            keyword: request.body.keyword
+        }
+        // 'Pro' 데이터베이스 연결
+        let db = (await connectDB).db('Pro')
+        // 글 업데이트. _id는 ObjectId로 변환하여 사용
+        let updateResult = await db.collection('post').updateOne(
+            { _id: new ObjectId(request.body._id) },
+            { $set: update }
+        );
+        
+        // 글이 성공적으로 업데이트되면 수정된 글을 보여주는 페이지로 리다이렉션
+        return response.redirect(302, `/${session.user.name}/${request.body._id}`)
+    }
+}
