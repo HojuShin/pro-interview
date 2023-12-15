f20ada60836c562a377027d730ad3dc4dd24d765
HojuShin <shin03188@naver.com>
2023-12-15 12:14:56 +0900

    feat: 글 삭제 서버 기능 개발

--- app/[name]/[id]/SeverBtn.js
+++ app/[name]/[id]/SeverBtn.js
@@ -3,21 +3,41 @@
 import { useRouter } from "next/navigation";
 
 export default function ServerBtn({ authorDb, matchdata }) {
+
     const router = useRouter();
+
     return (
         <div className="editSection">
-                <div className="editList">
-                    <div className="editList1">
-                        <button onClick={() => { router.push(`/${authorDb.name}/update/${matchdata._id}`) }}>
-                            <p>수정하기</p>
-                        </button>
-                    </div>
-                    <div className="editList2">
-                        <button className="deletebtn">
-                            <p>삭제하기</p>
-                        </button>
-                    </div>
+            <div className="editList">
+                <div className="editList1">
+                    <button onClick={() => { router.push(`/${authorDb}/update/${matchdata._id}`) }}>
+                        <p>수정하기</p>
+                    </button>
                 </div>
+                <div className="editList2">
+                    <button
+                        type="submit"
+                        className="deletebtn"
+                        onClick={() => {
+                            fetch('/api/delete', { method: 'DELETE', body: matchdata._id })
+                                .then((r) => {
+                                    // 응답이 성공적이지 않으면
+                                    if (!r.ok) {
+                                        throw new Error('fetch 네트워크 요청 혹은 서버 응답 에러');
+                                    }
+                                    return r.json();
+                                })
+                                .then((result) => {
+                                    if (result.redirectTo) {
+                                        window.location.href = result.redirectTo; //페이지 지정
+                                    }
+                                })
+                                .catch((error) => console.error('Fetch delete 에러:', error));
+                        }}>
+                        <p>삭제하기</p>
+                    </button>
+                </div>
+            </div>
         </div>
     )
 }
---
+++ pages/api/delete.js
@@ -0,0 +1,19 @@
+import { getServerSession } from "next-auth";
+import { authOptions } from "./auth/[...nextauth]";
+import { connectDB } from "@/db/dababase"
+import { ObjectId } from "mongodb";
+
+export default async function handler(req, res) {
+
+  let session = await getServerSession(req, res, authOptions)
+
+  if (req.method === 'DELETE') {
+
+    let db = (await connectDB).db('Pro');
+    await db.collection('post').deleteOne({ _id: new ObjectId(req.body) });
+
+    // 삭제 요청을 처리 후 완료되면 아래 해당 페이지로 이동
+    return res.status(200).json({ redirectTo: `/${session.user.name}` });
+  }
+  res.status(405).end(); // Method Not Allowed
+}
