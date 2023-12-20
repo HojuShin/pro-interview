3c7e65d15275fba43de289a046523f21cf0306f9
HojuShin <shin03188@naver.com>
2023-12-20 23:08:42 +0900

    add: 로딩화면 구현

---
+++ app/loading.js
@@ -0,0 +1,19 @@
+import Image from 'next/image'
+import loading from '@/public/loading.gif'
+
+export default function Loading() {
+    return (
+        <div
+            className='loadingSection'
+            style={{
+                width: '100%',
+                height: '100vh',
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                backgroundColor: '#1b1b1b',
+            }}>
+            <Image src={loading} alt="loading" id='loadingImg'></Image>
+        </div>
+    )
+}
