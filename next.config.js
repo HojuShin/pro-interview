c2b40280e97ca5343054460ddba73701f468f99d
HojuShin <shin03188@naver.com>
2024-01-01 21:04:00 +0900

    이미지 설정 권장사항 업데이트

--- next.config.js
+++ next.config.js
@@ -3,8 +3,11 @@
 const nextConfig = {
     images: {
         // Google 이미지 호스트
-        domains: ['lh3.googleusercontent.com'],
-    },
+        remotePatterns: [{
+            protocol: "https",
+            hostname: "lh3.googleusercontent.com",
+        }],
+    }
 };
 
 module.exports = nextConfig;
