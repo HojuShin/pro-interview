c36cdf5c654a61b1f93b7e6af7e6eee33889d87c
HojuShin <shin03188@naver.com>
2024-01-06 00:27:51 +0900

    fix: MongoDB useNewUrlParser 지원중단 경고 해결

--- db/dababase.js
+++ db/dababase.js
@@ -1,15 +1,16 @@
 import { MongoClient } from 'mongodb'
+
 const url = 'mongodb+srv://admin:ghwn2399@cluster0.xf4raeg.mongodb.net/?retryWrites=true&w=majority'
-const options = { useNewUrlParser: true }
-let connectDB
+let connectDB;
 
 if (process.env.NODE_ENV === 'development') {
     if (!global._mongo) {
-        global._mongo = new MongoClient(url, options).connect()
+        global._mongo = new MongoClient(url).connect();
     }
-    connectDB = global._mongo
+    connectDB = global._mongo;
 } else {
-    connectDB = new MongoClient(url, options).connect()
+    // 프로덕션 환경에서는 항상 새로운 MongoClient 생성
+    connectDB = new MongoClient(url).connect();
 }
 
-export { connectDB }
+export {connectDB} 
