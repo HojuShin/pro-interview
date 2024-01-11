2359486cf0f9e51003b38ebb2a15b0544cbaffe0
HojuShin <shin03188@naver.com>
2024-01-12 00:49:11 +0900

    feat: 비인증 사용자에 대한 페이지 접근 제한 미들웨어

--- middleware.js
+++ middleware.js
@@ -1,19 +1,32 @@
-
 import { cookies } from 'next/headers'
 import { NextResponse } from 'next/server';
 
-//  로그인 안된 사용자 write 페이지 접속 제한 미들웨어
-export async function middleware(request) {
+// 비인증 사용자의 페이지 접근을 제한하는 미들웨어
+// 브라우저 쿠키에서 세션 ID를 가져와 세션 ID가 없으면 'not-found' 페이지로 리다이렉트.
+export default async function Middleware(request) {
 
-  if (request.nextUrl.pathname.startsWith('/write')) {
-    // session 쿠키 유무 확인
-    const cookiesSession = cookies().get('next-auth.session-token');
-    console.log('쿠키 확인', cookiesSession)
+    // 브라우저 쿠키 가져오기
+    const cookieStore = cookies();
 
-    if (cookiesSession == undefined) {
-      // 쿠키 없으면 not found 페이지 보여주기
-      return NextResponse.redirect(new URL('/not-found', request.url));
+    // 쿠키에 저장된 세션 id를 담을 변수
+    let accessToken;
+
+    // 개발 환경 여부에 따라 쿠키 키를 확인
+    if (process.env.NODE_ENV === 'development') {
+        // dev 환경
+        accessToken = cookieStore.get('next-auth.session-token');
+    } else {
+        // production 환경
+        accessToken = cookieStore.get('__Secure-next-auth.session-token');
     }
-  }
 
-} 
+    // 쿠키에 세션id가 없으면 not-found 페이지로 리다이렉트
+    if (!accessToken) {
+        return NextResponse.redirect(new URL('/not-found', request.url));
+    }
+}
+
+// 접속 제한을 적용할 페이지 목록 설정
+export const config = {
+    matcher: ['/write', '/home', '/home/:path*', '/update/:path*']
+};
